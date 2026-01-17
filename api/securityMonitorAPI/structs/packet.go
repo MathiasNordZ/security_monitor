52fdcf485a2bb3b2a6d3f36b6c49d65251f1509a
Mathias Erik Nord <mathias.erik.nord@gmail.com>
2026-01-17 19:38:46 +0100

    Created struct for each packet and packet window.

--- api/securityMonitorAPI/structs/packet.go
+++ api/securityMonitorAPI/structs/packet.go
@@ -1,12 +1,20 @@
 package structs
 
+import "time"
+
 type Packet struct {
-	Timestamp string `json:"timestamp"`
-	Sensor    string `json:"sensor"`
-	SrcIP     string `json:"src_ip"`
-	SrcPort   string `json:"src_port"`
-	DstIp     string `json:"dst_ip"`
-	DstPort   int    `json:"dst_port"`
-	Protocol  string `json:"protocol"`
-	ByteCount int    `json:"byte_count"`
+	Timestamp time.Time `json:"timestamp"`
+	Sensor    string    `json:"sensor"`
+	SrcIP     string    `json:"src_ip"`
+	DstIP     string    `json:"dst_ip"`
+	SrcPort   int       `json:"src_port"`
+	DstPort   int       `json:"dst_port"`
+	Protocol  string    `json:"protocol"`
+	ByteCount int       `json:"byte_count"`
+}
+
+type PacketWindow struct {
+	WindowStart float64  `json:"window_start"`
+	WindowEnd   float64  `json:"window_end"`
+	Packets     []Packet `json:"packets"`
 }
